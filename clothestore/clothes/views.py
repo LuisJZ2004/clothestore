4a1e46684d7f207836a36fa6d3bd8c5aef571b61
beamerboy <luisjimenez10005@gmail.com>
2022-08-09 11:58:36 -0400

    all the filters works

--- clothestore/clothes/views.py
+++ clothestore/clothes/views.py
@@ -8,14 +8,31 @@ def clothes_by_gender(request, gender):
     clothes = Pledge.objects.filter(gender__in=gender)
     typeclothing = ClothingType.objects.filter(gender__in=gender)
     filter = request.GET.get("filter")
+    order = request.GET.get("order")
 
-    if filter:
-        clothes = Pledge.objects.filter(gender__in=gender, clothing_type__slug = filter)
+    orders = [
+        "-pub_date",
+        "-name",
+        "name",
+    ]
+
+    if filter or order:
+        if order and filter:
+            clothes = Pledge.objects.filter(gender__in=gender, clothing_type__slug = filter).order_by(order)
+        elif order:
+            clothes = clothes.order_by(order)
+        elif filter:
+            clothes = Pledge.objects.filter(gender__in=gender, clothing_type__slug = filter)
+        
 
     context = {
         'gender': gender,
         'pledges': clothes,
-        'type_clothing': typeclothing
+        'type_clothing': typeclothing,
+        'orders': orders,
+
+        'filter': filter,
+        'order': order,
     }
 
     return render(
