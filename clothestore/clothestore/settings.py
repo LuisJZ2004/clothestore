10a72c7d437da15cb38c4009378e7b1ac10fa4b4
beamerboy <luisjimenez10005@gmail.com>
2022-08-10 14:40:49 -0400

    app brands created and registered

--- clothestore/clothestore/settings.py
+++ clothestore/clothestore/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'clothes.apps.ClothesConfig',
+    'brands.apps.BrandsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
