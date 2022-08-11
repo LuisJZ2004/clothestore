b773bc50db93b883a59a28db67fa13089ad84281
beamerboy <luisjimenez10005@gmail.com>
2022-08-11 11:04:14 -0400

    the search system works

--- clothestore/search/urls.py
+++ clothestore/search/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import search_object
+
 app_name = "search"
 urlpatterns = [
-    
+    path("", search_object, name="search_object")
 ]
--- clothestore/search/views.py
+++ clothestore/search/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
+
+from clothes.models import Pledge
+from brands.models import Brand
 
 # Create your views here.
+
+def search_object(request):
+    search = request.GET["search-bar"]
+
+    if search:
+        pledges = Pledge.objects.filter(
+            Q(name__icontains = search) |
+            Q(description__icontains = search)
+        )
+        brands = Brand.objects.filter(
+            name__icontains = search
+        )
+    else:
+        return redirect(to="home:home_page")
+
+    context = {
+        'pledges': pledges, 
+    }
+
+    return render(
+        request=request,
+        template_name="search/search.html",
+        context=context,
+    )
