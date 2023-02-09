21a0fd83aad300ca33ae1b79ebb9e2fd1d88e01d
beamerboy <luisjimenez10005@gmail.com>
2023-02-09 14:44:25 +0000

    cart model created

--- clothestore/cart/models.py
+++ clothestore/cart/models.py
@@ -1,3 +1,21 @@
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# My apps
+from clothes.models import PledgeColorSet
+
+class Cart(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(to=PledgeColorSet)
+
+    def __str__(self) -> str:
+        return f"user '{self.user.username}' cart"
+
+    def total_price(self) -> int:
+        total_price = 0
+
+        for set in self.products:
+            total_price += set.price
+        
+        return total_price
